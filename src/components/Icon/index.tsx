b261d50a3d12287f1406b4e9d237da4b0ff37f91
Iury Brasileiro <iury.brasileiro@outlook.com.br>
2021-04-27 18:49:09 -0300

    chore: add icon font

---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { createIconSet } from 'react-native-vector-icons';
+
+import { useTheme } from 'styled-components';
+
+import awesomeicons from './assets/awesomeicons.json';
+
+export interface IIconProps {
+  name: string;
+  size?: number;
+  color?: string;
+}
+
+const RNIcon = createIconSet(awesomeicons, 'awesomeicons', 'awesomeicons.ttf');
+
+const Icon: React.FC<IIconProps> = ({ name, size, color }) => {
+  const theme = useTheme();
+
+  return (
+    <RNIcon
+      name={name}
+      size={size || 30}
+      color={color || theme.colors.global.secondary}
+    />
+  );
+};
+
+export default Icon;
